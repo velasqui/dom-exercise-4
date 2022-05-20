2e7fa24def80f2262b4d83ca7b84bc878e2946ba
Ken Cargill <ken.0.cargill@gmail.com>
2022-05-19 21:04:45 -0400

    for loop

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const selectedCoin = document.getElementById('coins');
+const coinAmt = document.getElementById('number-input');
 
 const moneyButton = document.getElementById('money-btn');
+
 moneyButton.addEventListener('click', (event) => {
     // console.log(selectedCoin.value);
-    const coinObject = document.createElement('div');
-    if (selectedCoin.value === 'Penny') {
-        coinObject.innerText = 'Penny';
-    } else if (selectedCoin.value === 'Nickel') {
-        coinObject.innerText = 'Nickel';
-    } else if (selectedCoin.value === 'Dime') {
-        coinObject.innerText = 'Dime';
-    } else {
-        coinObject.innerText = 'Quarter';
+    // console.log(coinAmt.value);
+    for (let i = 0; i< coinAmt.value; i++){
+        
+        const coinObject = document.createElement('div');
+        if (selectedCoin.value === 'Penny') {
+            coinObject.innerText = 'Penny';
+        } else if (selectedCoin.value === 'Nickel') {
+            coinObject.innerText = 'Nickel';
+        } else if (selectedCoin.value === 'Dime') {
+            coinObject.innerText = 'Dime';
+        } else {
+            coinObject.innerText = 'Quarter';
+        };
+    
+    
+        coinObject.style.cssText = 'width: 100px; height: 100px; background-color: lightgray; border-radius: 50%; text-align: center;';
+        const circleObject = document.createElement('div');
+        const circleDiv = document.getElementById('coins-div');
+        circleObject.append(coinObject);
+        circleDiv.append(circleObject);
     };
-    coinObject.style.cssText = 'width: 100px; height: 100px; background-color: lightgray; list-style: none; border-radius: 50%; text-align: center;';
-    const circleObject = document.createElement('div');
-    const circleDiv = document.getElementById('coins-div');
-    circleObject.append(coinObject);
-    circleDiv.append(circleObject);
-
-})
+});
