61c8ddb3d45aa23bf34e279060f5ed717e3fa126
bianca <bianca.gjokaj@gmail.com>
2022-05-19 20:19:39 -0400

    if loop

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ const selectedCoin = document.getElementById('coins');
 const moneyButton = document.getElementById('money-btn');
 moneyButton.addEventListener('click', (event) => {
     console.log(selectedCoin.value);
+    const coinObject = document.createElement('div');
+    if (selectedCoin.value === 'Penny') {
+        coinObject.innerText = 'Penny';
+    } else if (selectedCoin.value === 'Nickel') {
+        coinObject.innerText = 'Nickel';
+    } else if (selectedCoin.value === 'Dime') {
+        coinObject.innerText = 'Dime';
+    } else {
+        coinObject.innerText = 'Quarter';
+    };
 })
