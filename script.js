67e40e7ea2860f93625cf5b90afb8c14d55a630d
Ian Velasquez <ianvelasquez62@yahoo.com>
2022-05-19 20:44:27 -0400

    added functionality to button, circles will now be added to ui

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const selectedCoin = document.getElementById('coins');
 
 const moneyButton = document.getElementById('money-btn');
 moneyButton.addEventListener('click', (event) => {
-    console.log(selectedCoin.value);
+    // console.log(selectedCoin.value);
     const coinObject = document.createElement('div');
     if (selectedCoin.value === 'Penny') {
         coinObject.innerText = 'Penny';
@@ -13,4 +13,10 @@ moneyButton.addEventListener('click', (event) => {
     } else {
         coinObject.innerText = 'Quarter';
     };
+    coinObject.style.cssText = 'width: 100px; height: 100px; background-color: lightgray; list-style: none; border-radius: 50%; text-align: center;';
+    const circleObject = document.createElement('div');
+    const circleDiv = document.getElementById('coins-div');
+    circleObject.append(coinObject);
+    circleDiv.append(circleObject);
+
 })
